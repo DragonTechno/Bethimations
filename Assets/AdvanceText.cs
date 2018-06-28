c09f2482039a09043976c47f817c3b9274de00ed
DragonTechno <12izzy3@gmail.com>
2018-06-28 11:00:24 -0700

    All

---
+++ Assets/AdvanceText.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdvanceText : MonoBehaviour {
+    [TextArea]
+    public string[] slides;
+    int index;
+
+	// Use this for initialization
+	void Start () {
+        index = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GetComponent<Text>().text = slides[index];
+        if(Input.GetMouseButtonDown(0))
+        {
+            ++index;
+            if(index >= slides.Length)
+            {
+                index = 0;
+            }
+        }
+	}
+}
